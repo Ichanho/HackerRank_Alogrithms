855415c92d683bfc384f3587d5a7d1f5382a2fa4
LEE CHAN HO <supprpass15@gmail.com>
2022-12-05 23:37:12 +0900

    Solve pangrams problem

---
+++ pangrams.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pangrams' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function pangrams(s) {
+    // Write your code here
+    const arrS = s.toLowerCase().split('');
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".split('');
+
+    arrS.some((char)=>{
+      alphabet=alphabet.filter(x => x !== char);
+      if(alphabet.length===0) return true;
+    });
+    if (alphabet.length === 0) {
+      return 'pangram';
+    } else {
+      return 'not pangram';
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = pangrams(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
